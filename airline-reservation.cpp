21d61de9f00399510cf6c4281f5ee19a9a291435
sarthak_nawaria <sarthak.nawaria.41@gmail.com>
2022-09-08 14:26:55 +0530

    2

--- airline-reservation.cpp
+++ airline-reservation.cpp
@@ -1,9 +1,36 @@
 #include<iostream>
+#include<fstream>
+#include<iomanip>
+
 using namespace std;
+void mainMenu(); //Protyping a function
+
+class Managment{
+    private:
+
+    public:
+
+    Managment(){
+        mainMenu();
+    }
+
+
+};
+
+void mainMenu(){
+    int lchoice;
+    int schoice;
+    int back;
+
+    cout<<"\t              XYZ airlines \n "<<endl;
+    cout<<"\t______________Main Menu___________\n "<<endl;
+    cout<<"\t__________________________________"<<endl;
+    cout<<"\t\t\t\t\t\t\t\t\t\t\t\t"<<endl;
+}
   
 int main()
 {
-  
+  Managment Mobj;
   
  return 0;
 }
